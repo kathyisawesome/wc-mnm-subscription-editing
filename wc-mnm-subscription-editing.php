cc4ba5356a3a4b00161f231b99285dc8391fdf56
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-09-22 12:39:28 -0400

    version bump 1.0.0-rc.8

--- wc-mnm-subscription-editing.php
+++ wc-mnm-subscription-editing.php
@@ -2,7 +2,7 @@
 /**
  * Plugin URI: http://www.github.com/kathyisawesome/wc-mnm-subscription-editing
  * Plugin Name: WooCommerce Mix and Match - Subscription Editing
- * Version: 1.0.0-rc.7
+ * Version: 1.0.0-rc.8
  * Description: Mix and Match subscription container contents editing in the my account area, no cart/checkout
  * Author: Kathy Darling
  * Author URI: http://kathyisawesome.com/
@@ -35,7 +35,7 @@ if ( ! class_exists( 'WC_MNM_Subscription_Editing' ) ) :
 		/**
 		 * constants
 		 */
-		const VERSION = '1.0.0-rc.7';
+		const VERSION = '1.0.0-rc.8';
 		const REQ_MNM_VERSION = '2.4.0-beta.5';
 
 		/**
